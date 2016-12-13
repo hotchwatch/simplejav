347da813d3b7020f30c4aa52eb28265cac886793
Konstantin <Konstantin@10.194.248.47>
2016-12-13 15:24:30 +0100

    No change - only commit

---
+++ src/TestCircle.java
@@ -0,0 +1,16 @@
+
+public class TestCircle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Circle c1 = new Circle();
+		
+		System.out.println("The circle has radius of: " + c1.getRadius() + "and areaa of: " + c1.getArea()); 
+
+		Circle c2 = new Circle(2.0);
+		
+		System.out.println("The overriden circle has radius of: " + c2.getRadius() + "and areaa of: " + c2.getArea()); 
+	}
+
+}
